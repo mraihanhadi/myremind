0f7c124b82582437de52ad594a33b766cf763fdf
MNaufalRamdn <98917737+Radhan015@users.noreply.github.com>
2025-10-27 20:21:52 +0700

    Group info

--- MyRemind/app/src/main/java/com/example/myremind/navigation/NavRoute.kt
+++ MyRemind/app/src/main/java/com/example/myremind/navigation/NavRoute.kt
@@ -9,4 +9,7 @@ object NavRoute {
     const val PROFILE = "profile"
     const val GROUP = "group"
     const val ALARM_DELETE = "alarm_delete"
+
+    const val GROUP_INFO_PATTERN = "group_info/{groupId}"
+    fun groupInfo(groupId: String) = "group_info/$groupId"
 }
